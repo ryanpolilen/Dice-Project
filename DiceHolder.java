963995c3c0539f99b7063a0b251791e2a21d7975
gsakul21 <gsakul21@yahoo.com>
2018-12-05 10:35:15 -0500

    Modified DiceHolder.java
    Added the private instance variable respresenting container, is an ArrayList named holder. Added default constructor that initializes the holder. Added method taht will add a die to the holder if there are less than 6 dice in the holder.

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class DiceHolder{
 
+private ArrayList<Die> holder;
 
+public DiceHolder(){
+	holder = new ArrayList<>();
+}
 
+public int addDie(Die die){
+	if(holder.size() < 6){
+		holder.add(die);
+		return 1;
+	}
+	else
+		return -1;
+
+}
 
 }
